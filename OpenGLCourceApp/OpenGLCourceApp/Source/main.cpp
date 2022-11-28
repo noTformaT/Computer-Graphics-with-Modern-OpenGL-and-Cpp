f5a0a4c1085f8040b155c6154033810520ff77a8
noTformaT <noTformaT@gmail.com>
2022-11-28 22:59:07 +0200

    Commit first glew and glfw minimal app for cource

---
+++ OpenGLCourceApp/OpenGLCourceApp/Source/main.cpp
@@ -0,0 +1,84 @@
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+// windows dimentions
+const GLint WIDTH = 800, HEIGHT = 600;
+
+int main()
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        printf("GLFW Initialization FAILED!");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup GLFW window properties
+    
+    // OpenGL version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    
+    // Core profile = No Backwards Compatibility
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    // Allow forward compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", nullptr, nullptr);
+    if (!mainWindow)
+    {
+        printf("GLFW window creation FAILED!");
+        glfwTerminate();
+        return 1;
+    }
+
+    // Get buffer size information
+    int bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+    // Set context FOR GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+
+    if (glewInit() != GLEW_OK)
+    {
+        printf("GLEW Initialization FAILED!");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+
+    // Setup viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    // Loop until window closed
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        // Get and hanlde user input events
+        glfwPollEvents();
+
+        // Clear window
+        glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(mainWindow);
+    }
+
+    return 0;
+}
+
+// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
+// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
+
+// Советы по началу работы 
+//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
+//   2. В окне Team Explorer можно подключиться к системе управления версиями.
+//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
+//   4. В окне "Список ошибок" можно просматривать ошибки.
+//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
+//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
